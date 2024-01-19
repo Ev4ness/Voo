1fedfdaa317274cf637c6e7d98ed9318547f500a
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-01-19 22:52:13 +0700

    Update users.py

--- Music/core/users.py
+++ Music/core/users.py
@@ -7,8 +7,8 @@ from .logger import LOGS
 class UsersData:
     def __init__(self) -> None:
         self.DEVS = [
-            1432756163,  # ForGo10God
-            1874070588,  # ForGo10_God
+            835478438,  # ForGo10God
+            835478438,  # ForGo10_God
         ]
 
     async def sudo_users(self):
